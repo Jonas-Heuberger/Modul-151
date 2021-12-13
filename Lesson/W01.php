76bba8387bf8cfd9a8eb459955b0fb0da281a62c
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-12-13 11:17:18 +0100

    insert teachers code

---
+++ Lesson/W01.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="CSS/style.css">
+	<title>Document</title>
+</head>
+<body>
+	<div class="page">
+	<?php
+
+// Initialisierung
+$error = '';
+$firstname = $lastname = $email = $username = '';
+
+// Wurden Daten mit "POST" gesendet?
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  // Ausgabe des gesamten $_POST Arrays
+  echo "<pre>";
+  print_r($_POST);
+  echo "</pre>";
+
+  if(/* TODO firstname vorhanden, mindestens 1 Zeichen und maximal 30 Zeichen lang*/){
+    
+    $firstname = ""; // TODO Spezielle Zeichen Escapen > Script Injection verhindern
+  } else {
+    $error .= ""; // TODO Ausgabe Fehlermeldung
+  }
+
+  // TODO: Serverseitige Validierung: lastname analog firstname
+  // TODO: Serverseitige Validierung: email analog firstname
+  // TODO: Serverseitige Validierung: username analog firstname
+  // TODO: Serverseitige Validierung: password analog firstname
+}
+?>
+
+<div class="container">
+      <h1>Registrierung</h1>
+      <p>
+        Bitte registrieren Sie sich, damit Sie diesen Dienst benutzen können.
+      </p>
+      <?php
+        // Ausgabe der Fehlermeldungen
+        if(strlen($error)){
+          echo "<div class=\"alert alert-danger\" role=\"alert\">" . $error . "</div>";
+        }
+      ?>
+      <form action="" method="post">
+        <!-- TODO: Clientseitige Validierung: vorname -->
+        <div class="form-group">
+          <label for="firstname">Vorname *</label>
+          <input type="text" name="firstname" class="form-control" id="firstname"
+                  value="<?php echo $firstname ?>"
+                  placeholder="Geben Sie Ihren Vornamen an.">
+        </div>
+        <!-- TODO: Clientseitige Validierung: nachname -->
+        <div class="form-group">
+          <label for="lastname">Nachname *</label>
+          <input type="text" name="lastname" class="form-control" id="lastname"
+                  value="<?php echo $lastname ?>"
+                  placeholder="Geben Sie Ihren Nachnamen an">
+        </div>
+        <!-- TODO: Clientseitige Validierung: email -->
+        <div class="form-group">
+          <label for="email">Email *</label>
+          <input type="email" name="email" class="form-control" id="email"
+                  value="<?php echo $email ?>"
+                  placeholder="Geben Sie Ihre Email-Adresse an.">
+        </div>
+        <!-- TODO: Clientseitige Validierung: benutzername -->
+        <div class="form-group">
+          <label for="username">Benutzername *</label>
+          <input type="text" name="username" class="form-control" id="username"
+                  value="<?php echo $username ?>"
+                  placeholder="Gross- und Keinbuchstaben, min 6 Zeichen.">
+        </div>
+        <!-- TODO: Clientseitige Validierung: password -->
+        <div class="form-group">
+          <label for="password">Password *</label>
+          <input type="password" name="password" class="form-control" id="password"
+                  placeholder="Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute">
+        </div>
+        <button type="submit" name="button" value="submit" class="btn btn-info">Senden</button>
+        <button type="reset" name="button" value="reset" class="btn btn-warning">Löschen</button>
+      </form>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+	</div>
+</body>
+</html>

7d2afb19e2ef862684cae1e7d89154ec6af45a4f
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-02-07 08:04:15 +0100

    changed title

---  M151_Modularbeit_Jonas_Heuberger/edit.php
+++  M151_Modularbeit_Jonas_Heuberger/edit.php
@@ -14,7 +14,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
 <link rel="stylesheet" href="CSS/style.css">
-	<title>Document</title>
+	<title>Bearbeiten</title>
 </head>
 <body>
 <header>
---  M151_Modularbeit_Jonas_Heuberger/home.php
+++  M151_Modularbeit_Jonas_Heuberger/home.php
@@ -18,7 +18,7 @@ session_start();
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
 <link rel="stylesheet" href="CSS/style.css">
-	<title>Document</title>
+	<title>Home</title>
 </head>
 <body>
 
